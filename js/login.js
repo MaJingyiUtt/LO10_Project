c95fcb0c7afc7681c27d18a139dd544f3556aad2
MaJingyiUtt <48433363+MaJingyiUtt@users.noreply.github.com>
2019-05-13 12:34:50 +0200

    new bs + login finished

---
+++ js/login.js
@@ -0,0 +1,25 @@
+var userId;
+var userName;
+var userGivenName;
+var userFamilyName;
+var userEmail;
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    userId = profile.getId();
+    userName = profile.getName();
+    userGivenName = profile.getGivenName();
+    userFamilyName = profile.getFamilyName();
+    userEmail = profile.getEmail();
+
+    // The ID token you need to pass to your backend:
+    var id_token = googleUser.getAuthResponse().id_token;
+    console.log("ID Token: " + id_token);
+}
+
+function logOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+        console.log('User signed out.');
+    });
+    window.location.href = "index.html";
+}
--- js/main.js
+++ js/main.js
@@ -14,26 +14,5 @@ function onSignIn(googleUser) {
     // The ID token you need to pass to your backend:
     var id_token = googleUser.getAuthResponse().id_token;
     console.log("ID Token: " + id_token);
-    changeStatus();
-    user();
-}
-function logOut() {
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-        console.log('User signed out.');
-    });
-    document.getElementById("signout").style.display = "none";
-    document.getElementById("myCarousel").style.display = "block";
-    document.getElementById("userAccount").style.display = "none";
-    document.getElementById('username').innerText="LO10 projet";
-}
-
-function changeStatus() {
-    document.getElementById("signout").style.display = "block";
-    document.getElementById('username').innerText = userName;
-}
-function user() {
-    document.getElementById("myCarousel").style.display = "none";
-    document.getElementById("userAccount").style.display = "block";
-    document.getElementById('profil').innerText=" Nom : "+userFamilyName+"   "+"   Prénom : "+userGivenName+"    "+"   Email : "+userEmail+"   ";
+      window.location.href = "user.html";
 }

3e42bd9674763fbc45c8ae0a98ddb76f39fabce3
ranfang19 <ranfang19@gmail.com>
2019-05-21 18:49:45 +0200

    fix bug map and css firstLogin

--- js/script_map.js
+++ js/script_map.js
@@ -1,35 +1,28 @@
 var map,infowindow;
-var locations = [
-  ['Julie', 48.269398, 4.066733, 1],
-  ['Tom', 48.300058, 4.073061, 2]
-];
+var form = document.getElementById("form1");
+var isSubmitted = false;
+
+var locations;
+var placeSearch;
 
 var marker, i;
-// var infowindow = new google.maps.InfoWindow({
-//         content: "your"
-//       });
 
 function createMap () {
-  var options = {
-    center: { lat: 48.297023, lng: 4.073805 },
-    zoom: 10
-  };
-
-  map = new google.maps.Map(document.getElementById('map'), options);
-
-
-  var input = document.getElementById('search');
-  var searchBox = new google.maps.places.SearchBox(input);
-
-
-  map.addListener('bounds_changed', function() {
-    searchBox.setBounds(map.getBounds());
-  });
-
-  var markers = [];
-  
-  searchBox.addListener('places_changed', function () {
-    var places = searchBox.getPlaces();
+  if(!locations){
+    var options = {
+      center: { lat: 48.297023, lng: 4.073805 },
+      zoom: 10
+    };
+    map = new google.maps.Map(document.getElementById('map'), options);
+    var input = document.getElementById('search');
+    var searchBox = new google.maps.places.SearchBox(input);
+    map.addListener('bounds_changed', function() {
+      searchBox.setBounds(map.getBounds());
+    });
+    var markers = [];
+    
+    searchBox.addListener('places_changed', function () {
+      var places = searchBox.getPlaces();
 
     if (places.length == 0)
       return;
@@ -42,7 +35,6 @@ function createMap () {
     places.forEach(function(p) {
       if (!p.geometry)
         return;
-      
         markers.push(new google.maps.Marker({
         map: map,
         title: p.name,
@@ -51,8 +43,30 @@ function createMap () {
           url: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png"
         }
       }));
+      placeSearch = p.geometry.location;
+      if (p.geometry.viewport)
+        bounds.union(p.geometry.viewport);
+      else
+        bounds.extend(p.geometry.location);
+    }); 
+      map.fitBounds(bounds);
+    });
+  }
 
+  else {
+    var options = {
+      center: placeSearch,
+      zoom: 12
+    };
 
+    map = new google.maps.Map(document.getElementById('map'), options);
+      place = new google.maps.Marker({
+        position: placeSearch,
+        map: map,
+        icon: {
+          url: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png"
+        }
+      });
 
       for (i = 0; i < locations.length; i++) {
         marker = new google.maps.Marker({
@@ -64,22 +78,31 @@ function createMap () {
         var infowindow = new google.maps.InfoWindow();
         google.maps.event.addListener(marker,'click', (function(marker,content,infowindow){ 
           return function() {
-             infowindow.setContent(content);
-             infowindow.open(map,marker);
+            infowindow.setContent(content);
+            infowindow.open(map,marker);
           };
       })(marker,content,infowindow)); 
-
       }
+  }
+}
 
-      
+function submitFormData(){
+  // var type = form.type.value;
+  // var content = form.content.value;
+  // var search = form.search.value;
+  // var sexe = form.sexe.value;
+  // var prix_min = form.prix_min.value;
+  // var prix_max = form.prix_max.value;
 
-      if (p.geometry.viewport)
-        bounds.union(p.geometry.viewport);
-      else
-        bounds.extend(p.geometry.location);
-    });
+  locations = [
+    ['Julie', 48.269398, 4.066733, 1],
+    ['Tom', 48.300058, 4.073061, 2]
+  ];
 
-    
-    map.fitBounds(bounds);
-  });
-}  
+}
+
+function subForm(){
+  event.preventDefault();  // 取消按键的原始提交行为
+  submitFormData();  //启动监听提交按钮
+  createMap();
+}

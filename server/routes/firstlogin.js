ec7a34b23264acc1d3f967e17bbc703ffbd6eac7
BaiBlanc <33418651+BaiBlanc@users.noreply.github.com>
2019-05-31 17:12:58 +0200

    Finalisation of the demo

--- server/routes/firstlogin.js
+++ server/routes/firstlogin.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const router = express.Router()
+const Database = require("../utils/database")
+var db = new Database()
 
 //此处 根目录处于 /firstlogin/  详见 /server/index.js
 //得到post请求后执行下列命令：1. 显示收到了 2.显示数据 3.返回一个JSON
@@ -8,6 +10,8 @@ router.post('/', function (req, res) {
     console.info("request received")
     console.info(req.body)
 
+    //connect to db
+    
     res.send({ "message": "post successfully" })
 })
 

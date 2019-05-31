0c4067ade4b80b01ae5df032299005f8706ab990
ranfang19 <ranfang19@gmail.com>
2019-05-31 20:46:53 +0200

    postuler and google map search with address

--- js/postuler.js
+++ js/postuler.js
@@ -30,3 +30,24 @@ function checkLogin(){
     }
     return b;
 }
+
+function postuler(){
+    var form = document.getElementById("form_postuler");
+    var prix = form.prix.value;
+    var description = form.description.value;
+    console.log(prix);
+    console.log(description);
+}
+
+function check_prix(){
+    var form = document.getElementById("form_postuler");
+    var reg=/^[1-9]\d*$|^0$/; // 注意：故意限制了 0321 这种格式，如不需要，直接reg=/^\d+$/;
+    if(reg.test(form.prix.value)==true){
+        document.getElementById("warning").innerText="";
+        document.getElementById("submit").disabled=false;
+    }
+    else{
+        document.getElementById("warning").innerText="Entrer un nombre s'il vous plaît.";
+        document.getElementById("submit").disabled=true;
+    }
+}

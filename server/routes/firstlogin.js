bbaaa7a319a35f62e70e26f10416ec3cc9b092de
BaiBlanc <33418651+BaiBlanc@users.noreply.github.com>
2019-05-31 12:06:45 +0200

    add bodyParser && res=>JSON

--- server/routes/firstlogin.js
+++ server/routes/firstlogin.js
@@ -1,12 +1,11 @@
 const express = require('express')
 const router = express.Router()
 
-router.post('/', function(req, res){
+router.post('/', function (req, res) {
     console.info("request received")
     console.info(req.body)
-    
-    res.send(" post successfully!")
-    console.info(JSON.stringify(req.body))
+
+    res.send({ "message": "post successfully" })
 })
 
 module.exports = router

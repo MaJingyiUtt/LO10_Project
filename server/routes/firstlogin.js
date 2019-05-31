52babe18c8e4884ef664097d4d40e35a81901627
BaiBlanc <33418651+BaiBlanc@users.noreply.github.com>
2019-05-31 17:20:55 +0200

    Update firstlogin.js

--- server/routes/firstlogin.js
+++ server/routes/firstlogin.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const Database = require("../utils/database")
-var db = new Database()
+
+const db = new Database()
+const table = "login"
 
 //此处 根目录处于 /firstlogin/  详见 /server/index.js
 //得到post请求后执行下列命令：1. 显示收到了 2.显示数据 3.返回一个JSON
@@ -11,7 +13,8 @@ router.post('/', function (req, res) {
     console.info(req.body)
 
     //connect to db
-    
+    db.connect()
+    db.insert("")
     res.send({ "message": "post successfully" })
 })
 

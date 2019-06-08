efe7b583de73c6fda83d4d494238b313169a35d1
BaiBlanc <33418651+BaiBlanc@users.noreply.github.com>
2019-06-08 16:28:50 +0200

    Update imageUtil.js

--- server/utils/imageUtil.js
+++ server/utils/imageUtil.js
@@ -21,7 +21,7 @@ const detectFace = function (path,callback){
             if(info.length != 1){
                 message = "There is/are ",info.length," person(s) in your photo"
                 callback(false,message)
-            }else if(info[0].BoundingBox.Width*face.BoundingBox.Height<0.1){
+            }else if(info[0].BoundingBox.Width*info[0].BoundingBox.Height<0.1){
                 message = "Your face is not big enough in the photo(>10%)"
                 callback(false,message)
             }else if(info[0].Confidence<0.9){
